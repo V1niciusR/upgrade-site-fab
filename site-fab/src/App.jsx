a3574a7848e407b01ac450b748fe5edcfa508350
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-10 13:19:34 -0300

    Update App.jsx

--- site-fab/src/App.jsx
+++ site-fab/src/App.jsx
@@ -290,7 +290,7 @@ function App() {
             <div class="buttons">
               <a href="#" class="primary-btn">Know More</a>
               <a href="#" class="primary-btn outline external-link"> <span>Preview</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="#" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
@@ -315,7 +315,7 @@ function App() {
             <div class="buttons">
               <a href="#" class="primary-btn">Know More</a>
               <a href="#" class="primary-btn outline external-link"> <span>Preview</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="#" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
@@ -340,7 +340,7 @@ function App() {
             <div class="buttons">
               <a href="#" class="primary-btn">Know More</a>
               <a href="#" class="primary-btn outline external-link"> <span>Preview</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="#" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
