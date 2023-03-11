aa6a1c0d526cfac56f1123f350e09b614533f74a
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-11 20:52:41 -0300

    ...

--- site-fab/src/App.jsx
+++ site-fab/src/App.jsx
@@ -264,6 +264,93 @@ function App() {
 
 
 
+
+
+
+
+
+
+      <section class="portfolio" id="portfolio">
+
+        <h1 class="heading"> <span> my </span> portfolio </h1>
+
+        <div class="box-container">
+
+          <div class="box">
+            <img src="images/img-1.jpg" alt=""></img>
+            <h3> project 01 </h3>
+            <div class="icons">
+              <a href="#" class="fas fa-link"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-search"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <img src="images/img-2.jpg" alt=""></img>
+            <h3> project 02 </h3>
+            <div class="icons">
+              <a href="#" class="fas fa-link"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-search"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <img src="images/img-3.jpg" alt=""></img>
+            <h3> project 03 </h3>
+            <div class="icons">
+              <a href="#" class="fas fa-link"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-search"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <img src="images/img-4.jpg" alt=""></img>
+            <h3> project 04 </h3>
+            <div class="icons">
+              <a href="#" class="fas fa-link"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-search"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <img src="images/img-5.jpg" alt=""></img>
+            <h3> project 05 </h3>
+            <div class="icons">
+              <a href="#" class="fas fa-link"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-search"></a>
+            </div>
+          </div>
+
+          <div class="box">
+            <img src="images/img-6.jpg" alt=""></img>
+            <h3> project 06 </h3>
+            <div class="icons">
+              <a href="#" class="fas fa-link"></a>
+              <a href="#" class="fas fa-share"></a>
+              <a href="#" class="fas fa-search"></a>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+
       <section class="teams" id="equipe">
         <div class="container">
           <div class="section-title" data-aos="fade-down">
