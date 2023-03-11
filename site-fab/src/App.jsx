ec88a6b4638be4ed161dbec4a78da468bb506fbb
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-11 13:33:41 -0300

    algumas alterações

--- site-fab/src/App.jsx
+++ site-fab/src/App.jsx
@@ -277,7 +277,7 @@ function App() {
         <div class="item">
           <div class="left">
             <div class="img">
-              <img src="#" alt=""></img>
+              <img src="./src/assets/img/background/img_1.jpg" alt=""></img>
             </div>
           </div>
           <div data-aos="fade-up" class="right">
@@ -290,7 +290,7 @@ function App() {
             <div class="buttons">
               <a href="#" class="primary-btn">Saiba mais</a>
               <a href="#" class="primary-btn outline external-link"> <span>Veja</span>
-                <svg xmlns="#" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
@@ -302,7 +302,7 @@ function App() {
         <div class="item">
           <div class="left">
             <div class="img">
-              <img src="#" alt=""></img>
+              <img src="./src/assets/img/background/img_1.jpg" alt=""></img>
             </div>
           </div>
           <div data-aos="fade-up" class="right">
@@ -315,7 +315,7 @@ function App() {
             <div class="buttons">
               <a href="#" class="primary-btn">Saiba mais</a>
               <a href="#" class="primary-btn outline external-link"> <span>Veja</span>
-                <svg xmlns="#" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
@@ -327,7 +327,7 @@ function App() {
         <div class="item">
           <div class="left">
             <div class="img">
-              <img src="#" alt=""></img>
+              <img src="./src/assets/img/background/img_1.jpg" alt=""></img>
             </div>
           </div>
           <div data-aos="fade-up" class="right">
@@ -340,7 +340,7 @@ function App() {
             <div class="buttons">
               <a href="#" class="primary-btn">Saiba mais</a>
               <a href="#" class="primary-btn outline external-link"> <span>Veja</span>
-                <svg xmlns="#" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
